adb22cf1d8f6fe08c0706469e9534d8e45b0ec42
Terrance Woest <terrancewoest@gmail.com>
2018-06-30 11:44:36 -0700

    Added in Custom theme and set up laravel mix

---
+++ wp-content/themes/cornelsplumbing/webpack.mix.js
@@ -0,0 +1,16 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.setPublicPath('dist');
+
+mix.js('assets/js/app.js', 'app.js').sass('assets/sass/app.scss', 'app.css');
