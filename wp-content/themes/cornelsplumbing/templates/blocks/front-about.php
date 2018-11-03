db941828c8be771d637f2a977ebd20217bac7682
Terrance Woest <terrancewoest@gmail.com>
2018-11-03 06:50:24 -0700

    Removed extra about us section.

--- wp-content/themes/cornelsplumbing/templates/blocks/front-about.php
+++ wp-content/themes/cornelsplumbing/templates/blocks/front-about.php
@@ -13,28 +13,6 @@
                     <div class="card-body">
                         <h2 class="section-tag">About Cornel's Plumbing</h2>
 
-                        <h3 class="section-title mb-4">Meet Cornel and Mario. The Father/Son plumbing duo.</h3>
-
-                        <p class="section-text mb-0">
-                            Aenean lacinia bibendum nulla sed consectetur. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui.
-                        </p>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-        <div class="row align-items-center mb-5 pb-5">
-            <div class="col-sm-6 d-flex justify-content-center">
-                <img style="max-width: 100%;" src="<?php cp_asset('/images/cornel-old-picture.jpg'); ?>">
-            </div>
-
-            <div class="col-sm-6">
-
-                <div class="card card-shadow">
-                    <div class="card-body">
-                        <h2 class="section-tag">Cornel's Story</h2>
-
                         <h3 class="section-title mb-4">Hi! My name is Cornel.</h3>
 
                         <p class="section-text mb-0">My Plumbing Journey started in my home land of Romania. When I started plumbing I was equipped only with a bicycle to carry all of my tools, pipes, & fittings. More than 20 years ago we made a choice to leave our home and travel to an unknown land in the hopes of a better life. The services we offer today are services that can be trusted. They are tried and true and always come with the confidence of a job well done. Jobs you can trust are done right the first time. Although a job may seem hard, we like to keep a few words in mind. "The job might be hard, but we can do it"</p>
